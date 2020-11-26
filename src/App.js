97b59af09fce3aa0bf7695c50fb3056689127db4
Debojyoti Mukherjee <debojyotimukh@gmail.com>
2020-11-26 23:25:06 +0530

    one way databinding demo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      title: 'Hello from bridgelabz!'
+    }
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  render() {
+    return (
+      <div>
+        <h1>{this.state.title}</h1>
+      </div>
+    );
+  }
 }
+
 
 export default App;
