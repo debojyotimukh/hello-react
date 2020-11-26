d78c22ca8601f32b97216b8a7a796ee119e14c02
Debojyoti Mukherjee <debojyotimukh@gmail.com>
2020-11-26 23:44:30 +0530

    go to website when clicked logo using event binding

--- src/App.js
+++ src/App.js
@@ -7,13 +7,19 @@ class App extends React.Component {
     this.state = {
       title: 'Hello from bridgelabz!'
     }
+    this.url = "http://www.bridgelabz.com"
+  }
+
+  onClick = ($event) => {
+    console.log("Save button is clicked!", $event);
+    window.open(this.url, "_blank");
   }
 
   render() {
     return (
       <div>
         <h1>{this.state.title}</h1>
-        <img src={logo} alt="Bridbgelabz logo"></img>
+        <img src={logo} onClick={this.onClick} alt="Bridbgelabz logo"></img>
       </div>
     );
   }
